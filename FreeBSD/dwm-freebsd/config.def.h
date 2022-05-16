70207a12b4d4fea6d99c183953b1f210d4eb4f85
williamrollitt <75112729+williamrollitt@users.noreply.github.com>
2022-05-16 20:24:01 +0100

    Add files via upload

--- FreeBSD/dwm-freebsd/config.def.h
+++ FreeBSD/dwm-freebsd/config.def.h
@@ -2,15 +2,15 @@
 
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
-static const unsigned int gappx     = 8;        /* gaps between windows */
+static const unsigned int gappx     = 0;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
-static const int topbar             = 1;        /* 0 means bottom bar */
+static const int topbar             = 0;        /* 0 means bottom bar */
 static const char *fonts[]          = { "Perfect DOS VGA 437:size=10" };
 static const char dmenufont[]       = "Perfect DOS VGA 437:size=10";
 static const char col_gray1[]       = "#000000";
-static const char col_gray2[]       = "#ffffff";
-static const char col_gray3[]       = "#ffffff";
+static const char col_gray2[]       = "#aa0000";
+static const char col_gray3[]       = "#aa0000";
 static const char col_gray4[]       = "#ffffff";
 static const char col_cyan[]        = "#ff0000";
 static const char *colors[][3]      = {
