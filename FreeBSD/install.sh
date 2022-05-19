28e95e42c9654c50df17cee3ff74763232d8c8bb
williamrollitt <75112729+williamrollitt@users.noreply.github.com>
2022-05-19 17:35:40 +0100

    Add files via upload

--- FreeBSD/install.sh
+++ FreeBSD/install.sh
@@ -1,1 +1,5 @@
-exec pkg install doas vim vifm lynx git xorg neofetch feh mpv wifimgr
+# Installing all packages I use
+exec pkg install doas vim vifm lynx git xorg neofetch feh mpv wifimgr bash 
+
+# Adding bash as default shell for my user
+chsh -s /usr/local/bin/bash will
