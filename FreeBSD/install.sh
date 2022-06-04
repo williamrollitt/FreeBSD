b0006973f1aafecb9f766606df21b9d677692b45
williamrollitt <75112729+williamrollitt@users.noreply.github.com>
2022-06-04 10:28:40 +0100

    tor

--- FreeBSD/install.sh
+++ FreeBSD/install.sh
@@ -1,5 +1,5 @@
 # Installing all packages I use
-exec pkg install doas vim vifm lynx git xorg neofetch feh mpv wifimgr bash vimb tmux zathura zathura-pdf-plopper
+exec pkg install doas vim vifm lynx git xorg neofetch feh mpv wifimgr bash vimb tmux zathura zathura-pdf-plopper tor torsocks
 
 # Adding bash as default shell for my user
 chsh -s /usr/local/bin/bash will
